1caa07c75e6b7b0aa45c2132b5cc87c9e9ef5384
Asn-spidey <76788887+Asn-spidey@users.noreply.github.com>
2026-05-30 23:48:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,34 +5,39 @@
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
 
-
-from datetime import datetime
+from datetime import datetime as dt
 import pandas
 import random
 import smtplib
+from collections import defaultdict
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
-
-today = datetime.now()
-today_tuple = (today.month, today.day)
+today = (dt.now().month, dt.now().day)
 
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+
+bday_dict = defaultdict(list)
+
+for (index, row) in data.iterrows():
+    key = (row.month, row.day)
+    bday_dict[key].append(row)
+
+if today in bday_dict:
+    person = bday_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,1)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+        contents = contents.replace("[NAME]", person[0]["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    with smtplib.SMTP("smtp.mail.me.com", 587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.login(my_email, password)
         connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+            from_addr=my_email,
+            to_addrs=person[0]["email"],
+            msg=(
+                f"From: {my_email}\n"
+                f"Subject: Happy {dt.now().year - person[0]["year"]}th Birthday!\n"
+                f"{contents}"
+            )
         )
